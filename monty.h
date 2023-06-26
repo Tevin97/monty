ea2be3461b811f7b128f6956d3098c24bb62a94e
Tevin Murimi <Tevinmurimimwenda@gmail.com>
2023-06-25 17:02:52 -0700

    update

--- monty.h
+++ monty.h
@@ -47,6 +47,8 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void free_all(stack_t **stack);
+int delete_node(stack_t **stack, unsigned int index);
 
 
 
